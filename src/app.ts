ff38cdb3641bbe79a57f08713322e6b2022e8ca3
Taiki-nk <taikifate.0206@gmail.com>
2023-01-07 21:40:20 +0900

    ジェネリクステスト

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,10 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
+// const names:Array<string>=[]
 
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  // if (resultConversion === 'as-number') {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
-}
+// names[0].split(' ')
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
+const merge = <T extends {}, U>(objA: T, objB: U) => {
+  return Object.assign(objA, objB);
+};
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+const mergedObj = merge({ name: "Max" }, { age: 30 });
+console.log(mergedObj);
