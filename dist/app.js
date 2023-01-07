2c2f8ee02a981ea32e6d0e962d155bcab74f2db1
Taiki-nk <taikifate.0206@gmail.com>
2023-01-07 22:20:13 +0900

    ジェネリクス練習

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,50 @@
 "use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var merge = function (objA, objB) {
     return Object.assign(objA, objB);
 };
 var mergedObj = merge({ name: "Max" }, { age: 30 });
 console.log(mergedObj);
+var countAndDescribe = function (element) {
+    var descriptionText = element.length > 0 ? "\u5024\u306F".concat(element.length, "\u500B\u3067\u3059\u3002") : "値がありません";
+    return [element, descriptionText];
+};
+console.log(countAndDescribe("お疲れさまでした。"));
+console.log(countAndDescribe(["1こ", "２こ"]));
+var extactAndConvert = function (obj, key) {
+    return "Value: " + obj[key];
+};
+extactAndConvert({ name: "taiki" }, "name");
+var DataStrage = (function () {
+    function DataStrage() {
+        this.data = [];
+    }
+    DataStrage.prototype.addItem = function (item) {
+        this.data.push(item);
+    };
+    DataStrage.prototype.removeItem = function (item) {
+        console.log(this.data.indexOf(item));
+        if (this.data.indexOf(item) < 0)
+            return;
+        this.data.splice(this.data.indexOf(item), 1);
+    };
+    DataStrage.prototype.getItems = function () {
+        return __spreadArray([], this.data, true);
+    };
+    return DataStrage;
+}());
+var stringStrage = new DataStrage();
+stringStrage.addItem("test");
+stringStrage.addItem("test2");
+stringStrage.addItem("test3");
+stringStrage.removeItem("test2");
+console.log(stringStrage.getItems());
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -8,3 +8,51 @@ const merge = <T extends {}, U>(objA: T, objB: U) => {
 
 const mergedObj = merge({ name: "Max" }, { age: 30 });
 console.log(mergedObj);
+
+interface Lengthy {
+  length: number;
+}
+
+const countAndDescribe = <T extends Lengthy>(element: T) => {
+  const descriptionText =
+    element.length > 0 ? `値は${element.length}個です。` : "値がありません";
+  return [element, descriptionText];
+};
+
+console.log(countAndDescribe("お疲れさまでした。"));
+console.log(countAndDescribe(["1こ", "２こ"]));
+
+const extactAndConvert = <T extends Object, U extends keyof T>(
+  obj: T,
+  key: U
+) => {
+  return "Value: " + obj[key];
+};
+
+extactAndConvert({ name: "taiki" }, "name");
+
+class DataStrage<T> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    console.log(this.data.indexOf(item));
+    if (this.data.indexOf(item) < 0) return;
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const stringStrage = new DataStrage<string>();
+stringStrage.addItem("test");
+stringStrage.addItem("test2");
+stringStrage.addItem("test3");
+stringStrage.removeItem("test2");
+
+console.log(stringStrage.getItems());
